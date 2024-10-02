f08bb8c465005c5e14ae1d5c40570753ea7c2bb0
GravMAD <openai978396637@outlook.com>
2024-10-02 16:44:10 +0800

    init

--- docs/static/js/eval.js
+++ docs/static/js/eval.js
@@ -1,3 +1,55 @@
+// Dictionary for task names and their captions
+const noveltaskCaptions = {
+    "close_drawer": ['close bottom drawer', 'shut the bottom drawer', 'slide the middle drawer shut', 'close top drawer', 'shut the top drawer'],
+    "close_jar_banana": ['close the jar closer to the banana', 'screw on the jar lid that is closest to the banana', 'grasping the lid, lift it from the table and use it to seal the jar closer to the banana', 'pick up the lid from the table and put it on the jar closest to the banana', 'close the jar closer to the banana'],
+    "close_jar_distractor": ["close the lime jar.", "pick up the lid from the table and put it on the yellow jar.", "screw on the silver jar lid.", "grasping the lid, lift it from the table and use it to seal the gray jar.", "screw on the orange jar lid."],
+    "condition_block": ['stack 2 purple blocks and if blue block exists, add this blue block', 'build a tall tower out of 1 maroon cubes, and add a blue block if it exists', 'stack 2 maroon blocks and if blue block exists, add this blue block', 'build a tall tower out of 1 teal cubes, and add a blue block if it exists', 'stack 1 green blocks and if blue block exists, add this blue block'],
+    "meat_on_grill": ['put the chicken on the grill', 'pick up the chicken and place it on the grill', 'grill the steak', 'put the steak on the grill', 'pick up the steak and place it on the grill'],
+    "open_drawer_small": ["open the bottom drawer.", "open the bottom drawer.", "grip the middle handle and pull the middle drawer open.", "slide the middle drawer open.", "grip the top handle and pull the top drawer open."],
+    "stack_cups_blocks": ["Identify the most common color in the block pile, and stack the other cups on the cup that matches that color","Identify the most common color in the block pile, and stack the other cups on the cup that matches that color","Identify the most common color in the block pile, and stack the other cups on the cup that matches that color","Identify the most common color in the block pile, and stack the other cups on the cup that matches that color","Identify the most common color in the block pile, and stack the other cups on the cup that matches that color"],
+    "push_buttons_light": ['push the button with the same color as the light', 'press the button with the color of the light', 'press the button with the same color as the light', 'push the button with the same color as the light', 'press the button with the color of the light']
+};
+const taskStatus_voxposer = {
+    "close_drawer": ['fail', 'success', 'success', 'success', 'success'],
+    "close_jar_banana": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "close_jar_distractor": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "condition_block": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "meat_on_grill": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "open_drawer_small": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "stack_cups_blocks": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "push_buttons_light": ['fail', 'fail', 'fail', 'fail', 'fail'],
+};
+const taskStatus_act3d = {
+    "close_drawer": ['fail', 'fail', 'fail', 'fail', 'success'],
+    "close_jar_banana": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "close_jar_distractor": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "condition_block": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "meat_on_grill": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "open_drawer_small": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "stack_cups_blocks": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "push_buttons_light": ['fail', 'fail', 'fail', 'fail', 'fail'],
+};
+const taskStatus_3dda = {
+    "close_drawer": ['fail', 'fail', 'success', 'success', 'success'],
+    "close_jar_banana": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "close_jar_distractor": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "condition_block": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "meat_on_grill": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "open_drawer_small": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "stack_cups_blocks": ['fail', 'fail', 'fail', 'fail', 'fail'],
+    "push_buttons_light": ['fail', 'fail', 'fail', 'fail', 'fail'],
+};
+const taskStatus_gravmad = {
+    "close_drawer": ['success', 'success', 'success', 'success', 'success'],
+    "close_jar_banana": ['success', 'success', 'success', 'success', 'success'],
+    "close_jar_distractor": ['success', 'success', 'success', 'success', 'success'],
+    "condition_block": ['success', 'success', 'success', 'success', 'success'],
+    "meat_on_grill": ['success', 'success', 'success', 'success', 'success'],
+    "open_drawer_small": ['success', 'success', 'success', 'success', 'success'],
+    "stack_cups_blocks": ['success', 'success', 'success', 'success', 'success'],
+    "push_buttons_light": ['success', 'success', 'success', 'success', 'success'],
+};
+
 function updateOverallVideo() {
     const taskValue = document.getElementById('overall-menu-tasks').value.replace(/ /g, "_");
     const instanceValue = document.getElementById('overall-menu-instances').value;
@@ -17,12 +69,21 @@ function updateOverallVideo() {
         sourceElement.src = `${basePath}${taskValue}_demo_${instanceValue}.mp4`;
         videoElement.load(); // Reload video with new source
     }
+    // Update the title based on the selected task and instance
+    const titleElement = document.getElementById('novel-video-title');
+    titleElement.innerHTML = `<b>${noveltaskCaptions[taskValue][instanceValue - 1]}</b>`; // Use innerHTML to maintain bold
+    
+    // Update the labels with success/fail based on the selected task and instance
+    document.getElementById('voxposer-label').innerText = `VoxPoser (${taskStatus_voxposer[taskValue][instanceValue - 1]})`;
+    document.getElementById('act3d-label').innerText = `Act3D (${taskStatus_act3d[taskValue][instanceValue - 1]})`;
+    document.getElementById('3dda-label').innerText = `3D Diffuser Actor (${taskStatus_3dda[taskValue][instanceValue - 1]})`;
+    document.getElementById('gravmad-label').innerText = `GravMAD (${taskStatus_gravmad[taskValue][instanceValue - 1]})`;
 }
 
 
 // Dictionary for task names and their captions
 const taskCaptions = {
-    "close_jar": ["close the silver jar. (success)", "pick up the lid from the table and put it on the navy jar. (success)", "screw on the cyan jar lid. (success)", "grasping the lid, lift it from the table and use it to seal the white jar. (success)", "screw on the yellow jar lid. (success)"],
+    "close_jar": ["close the gray jar. (success)", "pick up the lid from the table and put it on the navy jar. (success)", "screw on the cyan jar lid. (success)", "grasping the lid, lift it from the table and use it to seal the white jar. (success)", "screw on the yellow jar lid. (success)"],
     "open_drawer": ["open the bottom drawer. (success)", "open the top drawer. (success)", "grip the bottom handle and pull the bottom drawer open. (success)", "slide the middle drawer open. (success)", "grip the middle handle and pull the middle drawer open. (fail)"],
     "meat_off_grill": ["take the steak off the grill. (success)", "pick up the chicken and place it next to the grill. (success)", "remove the chicken from the grill and set it down to the side. (fail)", "pick up the chicken and place it next to the grill. (success)", "take the steak off the grill. (success)"],
     "slide_block_to_color_target": ["slide the block to orange target. (success)", "slide the block onto the green square. (success)", "push the block until it is sitting on top of the magenta target. (success)", "slide the block towards the blue plane. (success)", "cover the yellow target with the block by pushing the block in its direction. (fail)"],
@@ -51,5 +112,5 @@ function updateSingleVideo() {
 
     // Update the title based on the selected task and instance
     const titleElement = document.getElementById('video-title');
-    titleElement.innerHTML = `<strong>${taskCaptions[taskValue][instanceValue - 1]}</strong>`; // Use innerHTML to maintain bold
+    titleElement.innerHTML = `<b>${taskCaptions[taskValue][instanceValue - 1]}</b>`; // Use innerHTML to maintain bold
 }
